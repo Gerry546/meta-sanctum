d62622de0328f232b0cd3091f327c6a4ca2b14ab
Tom Geelen <t.f.g.geelen@gmail.com>
2026-02-03 00:17:50 +0100

    devtool-pypi-helper: correct for the fact that now any recipe can be used with this script (see task.json) so if it contains python before hand it should be stripped
    Signed-off-by: Tom Geelen <t.f.g.geelen@gmail.com>

--- scripts/devtool-pypi-helper.sh
+++ scripts/devtool-pypi-helper.sh
@@ -12,6 +12,13 @@ ACTION="$1"
 RECIPE_NAME="$2"
 BUILD_DIR="build-$3"
 
+# Strip leading "python3" from recipe name (e.g. python3-foo -> foo)
+if [[ "$RECIPE_NAME" == python3* ]]; then
+    RECIPE_NAME_STRIPPED="${RECIPE_NAME#python3-}"
+    RECIPE_NAME_STRIPPED="${RECIPE_NAME_STRIPPED#python3}"
+fi
+
+
 # Source the Yocto build environment in the 'build' directory
 source "build/$BUILD_DIR/build/init-build-env"
 
@@ -22,11 +29,11 @@ fi
 echo "Running action: $ACTION for recipe: $RECIPE_NAME"
 if [ "$ACTION" == "add" ]; then
     # Run the add helper
-    devtool add $RECIPE_NAME https://pypi.org/project/$RECIPE_NAME/
+    devtool add $RECIPE_NAME https://pypi.org/project/$RECIPE_NAME_STRIPPED/
     exit 0
 fi
 if [ "$ACTION" == "finish" ]; then
     # Run the finish helper
-    devtool finish -r -f $RECIPE_NAME ../../sources/meta-homeassistant/recipes-devtools/python/
+    devtool finish -r -f $RECIPE_NAME ../../../../sources/meta-homeassistant/recipes-devtools/python/
     exit 0
 fi
